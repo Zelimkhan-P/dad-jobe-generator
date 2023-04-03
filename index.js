f5c0c720d42c7e333d0bf6f2f98fc5da94afac2a
Zelimkhan-P <chuvokhi@gmail.com>
2023-04-03 17:52:52 +0300

    Final

--- index.js
+++ index.js
@@ -12,9 +12,25 @@ const options = {
 const apiURL= "https://api.api-ninjas.com/v1/dadjokes?limit=1"
 
 async function getJoke () {
+
+try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+   btnEl.innerText = "Updating...";
     const response = await fetch(apiURL, options);
     const data = await response.json()
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+
     jokeEl.innerHTML = data[0].joke
+} catch (error) {
+    jokeEl.innerHTML = "An error happend, try again later"
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    console.log(error);
+}
+
+   
 }
 
 btnEl.addEventListener("click", getJoke)  
